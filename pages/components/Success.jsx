a2d233ae5726daa18f5c2275f255959fa396a68d
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-02-08 17:49:21 +0530

    Show success message

---
+++ pages/components/Success.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Success = () => {
+	return (
+		<div className="bg-green-600 px-10 py-5 rounded-full">
+			<h1 className="text-white text-xl font-semibold">
+				Welcome to the fam!👋
+			</h1>
+			<p className="text-white text-lg font-bold">
+				An email is on the way! It might be in spam folder.
+			</p>
+		</div>
+	);
+};
+
+export default Success;
